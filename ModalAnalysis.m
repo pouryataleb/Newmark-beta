23322316bdc06554c098905bea8ca75a6b027f2a
Pourya Talebsafa <40524988+pouryataleb@users.noreply.github.com>
2022-06-11 22:17:53 +0430

    Add files via upload

---
+++ ModalAnalysis.m
@@ -0,0 +1,33 @@
+function [M,K,C,P,phi,w]= ModalAnalysis(M,K,C,P,lambda)
+% Modal Analysis of the System 
+%--------------------------------------------------------------------------
+% Purpose : To do the Modal Analysis of the system
+%
+% Synopsis : [M,K,C,P] = ModalAnalysis(M,K,C,P,lambda]
+%
+% Variable Description :
+%           M - Mass Matrix
+%           K - Stiffness Matrix
+%           C - Damping Matrix
+%           P - Force MAtrix
+%           lambda - number of modes to be considered
+%           phi - Reduced Normal modes of the system
+%--------------------------------------------------------------------------
+
+ % Modal Analysis 
+[V,D]=eig(K,M);
+[W,k]=sort(diag(D));
+V=V(:,k); 
+Factor=diag(V'*M*V);
+Phi=V*inv(sqrt(diag(Factor)));
+Omega=diag(sqrt(Phi'*K*Phi)); 
+
+% selecting only first two Natural Frequencies and Mode shapes
+w = diag(Omega(1:lambda)) ;
+phi = Phi(:,1:lambda) ;
+
+% Reducing the Degree's of Freedom of M, K and P
+M = phi'*M*phi ;
+C = phi'*C*phi ;
+K = phi'*K*phi ;
+P = phi'*P ;
